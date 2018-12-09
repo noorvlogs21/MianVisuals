9ad5c2e5be2ab1e4a0aaf69e1dc1e656e4539c33
Noor Uddin <noorvlogs21@gmail.com>
2018-12-09 20:19:42 +0000

    Further Changes to the UI

---
+++ js/materializeJS/matInit.js
@@ -0,0 +1,8 @@
+//FAB Button
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
